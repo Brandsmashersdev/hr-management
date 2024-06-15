30ba2f82198a34cc2831e0eae9c324a6806d3adb
Mann14 <mannmittal622@gmail.com>
2024-06-15 10:31:25 +0530

    Update in profile section

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,26 +1,53 @@
 // pages/login.js
 import { useState } from 'react';
 import Link from 'next/link';
+import axios from 'axios';
+import Layout from './Layout'
+import Profile from './Employee-dashboard/[username]/profile'
+
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () => {
+      setPasswordVisible(!passwordVisible);
+  }
+  const [message, setMessage] = useState('');
 
   const handleLogin = async () => {
-    const response = await fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password }),
-    });
-
-    if (response.ok) {
-      // Redirect to a protected page on successful login
-      window.location.href = '/HR-dashboard/Profile';
-    } else {
-      // Handle login failure
-      console.error('Login failed');
+    try {
+      const response = await axios.post('/api/login', { username, password });
+      if (response.data.message === 'ok') {
+        window.location.href = `/Employee-dashboard/${username}/profile`;
+          } else {
+            // Handle login failure
+            console.error('Login failed');
+          }
+    } catch (error) {
+      if (error.response) {
+        setMessage(error.response.data.message);
+      } else {
+        setMessage('An unexpected error occurred');
+      }
     }
   };
+
+
+  // const handleLogin = async () => {
+  //   const response = await fetch('/api/login', {
+  //     method: 'POST',
+  //     headers: { 'Content-Type': 'application/json' },
+  //     body: JSON.stringify({ username, password }),
+  //   });
+  //   // console.log(response);
+  //   if (response.ok) {
+  //     window.location.href = '/Layout';
+  //   } else {
+  //     // Handle login failure
+  //     console.error('Login failed');
+  //   }
+  // };
 
   return (
     <div className='login_background' id='login'>
@@ -31,21 +58,23 @@ const Login = () => {
         <div className='login_form_input'>
           <div className='login_head'><h3>Login</h3></div>
           <div>
-            <form action="" id='login_form_data'>
              <div className='login_form_inputs'>
                 <label htmlFor="username">Username</label>
                 <input type="text" id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
              </div>
              <div className='login_form_inputs'>
                 <label htmlFor="password">Password</label>
-                <input type="password" id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type={passwordVisible ? 'text' : 'password'} id='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <i onClick={togglePasswordVisibility} className="eye-icon">
+                {passwordVisible ? '👁️‍🗨️' : '👁️'}
+                </i>
                 <Link href="/change_password" className='for_password'>Forget password</Link>
              </div>
              <div className='login_form_inputs'>
-              <button onClick={handleLogin}>Login</button>
+              <button onClick={handleLogin} className='logout'>Login</button>
              </div>
-             <div><p style={{textAlign:"center"}}>Unable to login? Kindly connect with <u>IT Support Team</u> </p></div>
-            </form>
+             <div className='help'><p style={{textAlign:"center", margin:"30px"}}>Unable to login? Kindly connect with <u>IT Support Team</u> </p></div>
+             <p>{message}</p>
       </div>
         </div>
         <div className='login_img'>
